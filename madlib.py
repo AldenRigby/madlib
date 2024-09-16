1b86ebf1965f33bdfc21689f18b0a24904c8da6f
AldenRigby <alden.rigby@ucas-edu.net>
2024-09-16 11:31:11 -0600

    aes mnfvsjdrubfhlakwef

--- madlib.py
+++ madlib.py
@@ -6,3 +6,10 @@ print("owo")
 print("grawweh")
 
 print(":3")
+temp = "0 1 2 3 4 5 6 7 8 9".split(" ")
+temp2= []
+for i in temp:
+    temp2.append(int(i))
+import random
+random.shuffle(temp2)
+print(temp2)
